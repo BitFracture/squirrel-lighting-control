869b9b1dc11c5442b288ce5bceb43780001ef19e
StanleyMango <StanleyKMugo@gmail.com>
2017-12-01 11:22:23 -0800

    Working pressure code

---
+++ sketch_esp8266_node_pressure/sketch_esp8266_node_pressure.ino
@@ -0,0 +1,122 @@
+/**
+   The Flying Squirrels: Squirrel Lighting Controller
+   Node:     2/Client: Sun Meter
+   Hardware: ESP8266-01[S]
+   Purpose:  Monitor exterior light levels, report to server.
+   Author:   Erik W. Greif
+   Date:     2017-10-14
+*/
+
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiAP.h>
+#include <ESP8266WiFiGeneric.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266WiFiScan.h>
+#include <ESP8266WiFiSTA.h>
+#include <ESP8266WiFiType.h>
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
+
+#include <TcpClientRegistrar.h>
+#include <CommandInterpreter.h>
+#include <Pcf8591.h>
+
+const char* WIFI_SSID = "SQUIRREL_NET";
+const char* WIFI_PASS = "wj7n2-dx309-dt6qz-8t8dz";
+bool reconnect = true;
+
+WiFiEventHandler disconnectedEventHandler;
+TcpClientRegistrar registrar;
+CommandInterpreter ioCmd;
+WiFiClient* clientIoControl = NULL;
+WiFiServer listeningConnection(23);
+Pcf8591 ioChip(&Wire);
+
+void setup() {
+  Serial.begin(9600);
+  delay(500);
+  Serial.print("Initialized\n");
+  Wire.begin(2, 0);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  disconnectedEventHandler = WiFi.onStationModeDisconnected(&triggerReconnect);
+
+  ioCmd.assign("g", getPressure);
+
+  listeningConnection.begin();
+  registrar.assign("iocontrol", &clientIoControl);
+}
+
+void loop() {
+  //*
+  //Do nothing until we are connected to the server
+  //handleReconnect(); //-------------------------------------*********
+
+  // Client recv
+  registrar.handle(listeningConnection);
+
+  //Handle commands
+  ioCmd.handle(Serial);
+  if (clientIoControl != NULL && clientIoControl->connected())
+    ioCmd.handle(*clientIoControl);
+  
+  
+  Serial.println(ioChip.read(0, 0));
+  //**/
+
+  /*
+    //Capture audio level from chip
+    static uint8_t level = 0;
+    static uint32_t thisSampleTime, lastSampleTime = 0;
+    thisSampleTime = millis();
+    if (thisSampleTime - lastSampleTime > 5) {
+      lastSampleTime = thisSampleTime;
+
+
+      Serial.print(ioChip.read(0, 0));
+      Serial.print(" ");
+
+      Serial.print("*");
+      Serial.print(ioChip.read(0, 2));
+      Serial.print(" ");
+    }
+    //**/
+}
+
+void triggerReconnect(const WiFiEventStationModeDisconnected& event) {
+
+  reconnect = true;
+}
+
+void handleReconnect() {
+  //TODO: Reconnect if server TCP connection lost
+  while (reconnect) {
+    //Wait for wifi for 5 seconds
+    Serial.print("Wait\n");
+    for (int i = 10; WiFi.status() != WL_CONNECTED && i > 0; i--) {
+      delay(500);
+    }
+    if (WiFi.status() != WL_CONNECTED) {
+      continue;
+    }
+
+    //Try to connect persistently to squirrel
+    WiFiClient registerClient;
+    if (TcpClientRegistrar::connectClient(
+          registerClient, IPAddress(192, 168, 3, 1), 23, "pressure", false))
+      reconnect = false;
+  }
+}
+
+void getPressure(Stream& reply, int argc, const char** argv) {
+  static const int BUFFER_LEN = 5;
+  static char buffer[BUFFER_LEN];
+  sprintf(buffer, "%i\n", ioChip.read(0, 0));
+  reply.print(buffer);
+}
+
